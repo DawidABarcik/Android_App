d6ac4c575ffbf170b7cab886eeed2f1b8cb48cc4
DawidABarcik <dawid.a.barcik@gmail.com>
2021-02-01 13:27:23 +0000

    Added Hamburger Functionality (Fragment Viewer)

--- app/src/main/java/com/example/Android_App/RegisterActivity.java
+++ app/src/main/java/com/example/Android_App/RegisterActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,51 +19,102 @@ public class RegisterActivity extends AppCompatActivity {
     TextView mTextViewLogin;
     DatabaseHelper db;
 
+    int score = 0;
+    // boolean indicating if password has an upper case
+    boolean upper = false;
+    // boolean indicating if password has a lower case
+    boolean lower = false;
+    // boolean indicating if password has at least one digit
+    boolean digit = false;
+    // boolean indicating if password has a least one special char
+    boolean specialChar = false;
+    // boolean indication if password meets the requirements
+    boolean usable = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.fragment_register);
 
         db = new DatabaseHelper(this);
-        mTextUsername = (EditText)findViewById(R.id.edittext_username);
-        mTextPassword = (EditText)findViewById(R.id.edittext_password);
-        mTextConfirmPassword = (EditText)findViewById(R.id.edittext_confirm_password);
+        mTextUsername = (EditText) findViewById(R.id.edittext_username);
+        mTextPassword = (EditText) findViewById(R.id.edittext_password);
+        mTextConfirmPassword = (EditText) findViewById(R.id.edittext_confirm_password);
         mButtonRegister = (Button) findViewById(R.id.button_register);
         mTextViewLogin = (TextView) findViewById(R.id.textview_login);
-        mTextViewLogin.setOnClickListener(new View.OnClickListener(){
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View view){
-                Intent loginIntent = new Intent(RegisterActivity.this,LoginActivity.class);
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(loginIntent);
             }
         });
 
-        mButtonRegister.setOnClickListener(new View.OnClickListener(){
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 String user = mTextUsername.getText().toString().trim();
                 String password = mTextPassword.getText().toString().trim();
                 String confirm_password = mTextConfirmPassword.getText().toString().trim();
                 Boolean res = db.checkUser(user, password);
-                if (res == false){
-                    if (password.equals(confirm_password)){
-                        long val =  db.addUser(user,password);
-                        if (val > 0){
-                            Toast.makeText(RegisterActivity.this,"You have registered successfully",Toast.LENGTH_SHORT).show();
-                            Intent moveToLogin = new Intent(RegisterActivity.this, LoginActivity.class);
-                            startActivity(moveToLogin);}
-                        else{
-                            Toast.makeText(RegisterActivity.this,"Passwords are not matching",Toast.LENGTH_SHORT).show();
+                if (res == false) {
+                    if (usable == true) {
+                        if (password.equals(confirm_password)) {
+                            long val = db.addUser(user, password);
+                            if (val > 0) {
+                                Toast.makeText(RegisterActivity.this, "You have registered successfully", Toast.LENGTH_SHORT).show();
+                                Intent moveToLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(moveToLogin);
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Passwords are not matching", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Passwords are not matching", Toast.LENGTH_SHORT).show();
                         }
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Password does not meet the requirements", Toast.LENGTH_SHORT).show();
                     }
-                    else{
-                        Toast.makeText(RegisterActivity.this,"Passwords are not matching",Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else {
+                } else {
                     Toast.makeText(RegisterActivity.this, "User already exists", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+
+
+    public void passwordStrength (String password) {
+
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+
+            if (!specialChar && !Character.isLetterOrDigit(c)) {
+                specialChar = true;
+            } else {
+                if (!digit && Character.isDigit(c)) {
+                    digit = true;
+                } else {
+                    if (!upper || !lower) {
+                        if (Character.isUpperCase(c)) {
+                            upper = true;
+                        } else {
+                            lower = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void passwordUsable ()
+    {
+        if (upper == true && lower == true && digit == true && specialChar == true)
+        {
+            usable = true;
+        }
+        else
+        {
+            usable = false;
+        }
     }
 }
